793c845423560cacf604da659ac48bc868870ef8
adarshNitj <adarshbajpai0907@gmail.com>
2023-08-03 17:49:09 +0530

    multiply added and diff updated

---
+++ Diff.java
@@ -0,0 +1,9 @@
+public class Diff {
+            public static void main(String args[]){
+                int a  = 10;
+                int b = 20;
+
+                int d = b-a;
+                System.out.println("Difference = " + d);
+            }
+}
---
+++ Multiply.java
@@ -0,0 +1,6 @@
+public class Multiply {
+    int a =7;
+    int b=8;
+    int m= a*b;
+    System.out.println("Product="+ m);
+}
